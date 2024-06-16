b8811117c511c291f0c96a52943b63ef2a4222c7
Mihail Test <misha.lucenko17@gmail.com>
2024-06-16 15:13:31 +0300

    Isparvlen commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        BmiService service = new BmiService ();
+
+        BmiService service = new BmiService();
         double weightKilogram = 82;
         double heightMetre = 1.72;
+
         int index = service.calculate(weightKilogram, heightMetre);
         System.out.println(index);
-        }
     }
+}
